479debb3f8fabc0186b5488281fd976d43ed29ce
Bogdan Călin <calin.bogdan15@gmail.com>
2017-11-24 20:25:10 +0200

    Added Text css modules

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -2,24 +2,25 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import Container from './Container';
 import NavigationItem from './NavigationItem';
+import styles from './NavigationBar.css';
 
 export default class NavigationBar extends React.Component {
     render() {
         return (
-            <div className="navbar">
+            <div className={styles.navbar}>
                 <Container>
-                <div className="brand">
-                    CALIN BOGDAN
-                </div>
-                {/* links */}
-                <div>
-                    <NavigationItem route="/contact">
-                        JOB HISTORY & EDUCATION
-                    </NavigationItem>
-                    <NavLink to="/contact">
-                        CONTACT
-                    </NavLink>
-                </div>              
+                    <div className="brand">
+                        CALIN BOGDAN
+                    </div>
+                    {/* links */}
+                    <div>
+                        <NavigationItem route="/contact">
+                            JOB HISTORY & EDUCATION
+                        </NavigationItem>
+                        <NavigationItem route="/contact">
+                            CONTACT
+                        </NavigationItem>
+                    </div>              
                 </Container>
             </div>
         );
--- src/components/pages/Main.js
+++ src/components/pages/Main.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Container from './../Container';
+import styles from './../Text.css';
 
 export default class Main extends React.Component {
     render() {
         return (
             <Container>
-                <h1>I'm a young developer from Romania creating first class web and mobile applications.</h1>
-                <h4>For more than 4 years I've been coding as a hobby using mostly C#, JavaScript and SQL. I've even managed to sell some software.</h4>
+                <p className={styles.headline}>I'm a young developer from Romania creating first class web and mobile applications.</p>
+                <p className={styles.lead}>For more than 4 years I've been coding as a hobby using mostly C#, JavaScript and SQL. I've even managed to sell some software.</p>
             </Container>
         )
     }
