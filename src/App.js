98fac551275b3327a0e2f52be05df6adbd118ce1
Bogdan Călin <calin.bogdan15@gmail.com>
2017-11-24 19:51:06 +0200

    Basic scheme of the website

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Container from './components/Container';
 
 class App extends Component {
   render() {
     return (
-      <div className="container">
-      </div>
+
+      <BrowserRouter>
+        <div>
+
+          {/* navbar */}
+          <div className="navbar">
+            <Container>
+              <div className="brand">
+                CALIN BOGDAN
+              </div>
+              {/* links */}
+              <div>
+                JOB HISTORY
+              </div>
+              <div>
+                CONTACT
+              </div>
+            </Container>
+          </div>
+          <Switch>
+            <Route exact path="/" component={Main}/>
+            <Route path="/contact" component={Contact}/>
+          </Switch>    
+          
+        </div>          
+      </BrowserRouter>  
+      
     );
   }
 }
