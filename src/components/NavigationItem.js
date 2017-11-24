cfb5edcba2e84000b97775f0d620e60ab158866e
Bogdan Călin <calin.bogdan15@gmail.com>
2017-11-24 20:37:58 +0200

    Fixed some risky descriptions

--- src/components/NavigationItem.js
+++ src/components/NavigationItem.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
+import styles from './NavigationItem.css';
 
 class NavigationItem extends React.Component {
     render() {
         return (
-            <div>
-                <NavLink to={this.props.route}>
-                    {this.props.children}
-                </NavLink>
-            </div>
+            <NavLink className={styles.navlink} to={this.props.route}>
+                {this.props.children}
+            </NavLink>
         )
     }
 }
--- src/components/pages/Main.js
+++ src/components/pages/Main.js
@@ -6,7 +6,7 @@ export default class Main extends React.Component {
     render() {
         return (
             <Container>
-                <p className={styles.headline}>I'm a young developer from Romania creating first class web and mobile applications.</p>
+                <p className={styles.headline}>I'm a young developer from Romania which is in love with coding and Metallic Blue painted cars.</p>
                 <p className={styles.lead}>For more than 4 years I've been coding as a hobby using mostly C#, JavaScript and SQL. I've even managed to sell some software.</p>
             </Container>
         )
